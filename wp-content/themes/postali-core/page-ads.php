b06217163458480ee46450c9b8e9160e151218c2
Md. Rakibul Islam <rakib.wdev@gmail.com>
2026-03-04 14:09:40 +0600

    footed end

--- wp-content/themes/postali-core/page-ads.php
+++ wp-content/themes/postali-core/page-ads.php
@@ -698,20 +698,45 @@
                     <img class="footor_log" src="<?php echo esc_url($logo['url']); ?>"
                         alt="<?php echo esc_attr($logo['alt']); ?>">
                 <?php endif; ?>
-                <p>© <time datetime="2026">2026</time> Dads.Law — Dedicated Fathers' Rights Lawyers in Tulsa, Oklahoma. This site is for informational purposes only and does not constitute legal advice.</p>
+                <p>
+                    © <time datetime="<?php echo date('Y'); ?>">
+                        <?php echo date('Y'); ?>
+                    </time>
+                    <?php the_field('p11_title'); ?>
+                </p>
             </div>
         </div>
 
 
         <div class="footor_stikey">
             <div class="stikey_inner">
-                <p class="f_text">Consultation — Talk to a Men's Divorce Attorney Now</p>
-                <a href="tel:9189849424" class="third_btn_icon">
+                <p class="f_text"><?php the_field('sticky_title'); ?></p>
+                <!-- <a href="tel:9189849424" class="third_btn_icon">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-phone w-5 h-5">
                         <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path>
                     </svg>
                     <p>Call (918) 984-9424</p>
-                </a>
+                </a> -->
+                <?php
+                $phone_call = get_field('sticky_phone');
+                if ($phone_call && !empty($phone_call['url'])) :
+
+                    // Remove http/https if added by ACF
+                    $phone = preg_replace('#^https?://#', '', $phone_call['url']);
+
+                    // Keep only numbers and +
+                    $phone_clean = preg_replace('/[^0-9+]/', '', $phone);
+                ?>
+
+                    <a href="tel:<?php echo esc_attr($phone_clean); ?>" class="third_btn_icon">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-phone w-5 h-5">
+                            <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path>
+                        </svg>
+
+                        <?php echo esc_html($phone_call['title']); ?>
+                    </a>
+
+                <?php endif; ?>
             </div>
         </div>
     </footer>
